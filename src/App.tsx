daa0d025268f9aa377b45a4ae0705cb061c5388e
kanakuii <naomidao814@gmail.com>
2025-10-31 00:56:13 -0500

    small import tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './pages/login.tsx'
+import Login from './pages/Login'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import '../App.css'
-import Button from '../components/button'
+import Button from '../components/Button'
 
 export default function Login() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
         <div className="background">
