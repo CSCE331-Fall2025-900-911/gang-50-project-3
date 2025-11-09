8acd5fcbb39cd1a0196b58cc8ddc12a553602d7b
malcolmtroy <malcolmtroy01@tamu.edu>
2025-11-09 15:56:06 -0600

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -21,8 +21,16 @@ export default function Login() {
             <div className="backgroundBox">
                 <div>
                     <img src="/sharetealogo.png" className="logo" alt="ShareTea logo" />
+                    <h2>Login with Employee ID</h2>
                 </div>
-            </div>
+                <div>
+                    <h3>Employee ID</h3>
+                    <input type="text" placeholder="Employee ID" className="input" />
+                    <h3>Last Name</h3>
+                    <input type="text" placeholder="Last Name" className="input" />
+                </div> 
+                <Button label="Login" onClick={() => {}} />
+            </div> 
       </div>
       <GoogleLogin
         onSuccess={handleLoginSuccess}
