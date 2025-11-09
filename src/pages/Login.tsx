ac4af0e5a57721cd9c783529d79d5b00fbcd2df9
malcolmtroy <malcolmtroy01@tamu.edu>
2025-11-09 15:14:57 -0600

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,19 @@
 import '../App.css'
 import Button from '../components/Button'
+import { GoogleLogin } from '@react-oauth/google'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function Login() {
+
+  const navigate = useNavigate()
   return (
     <>
-        <div className="background">
-            <div className="backgroundBox">
-                <div>
-                    <img src="../../public/sharetealogo.png" className="logo" alt="ShareTea logo" />
-                    <h2>Login with Employee ID</h2>
-                </div>
-                <div>
-                    <h3>Employee ID</h3>
-                    <input type="text" placeholder="Employee ID" className="input" />
-                    <h3>Last Name</h3>
-                    <input type="text" placeholder="Last Name" className="input" />
-                </div> 
-                <Button label="Login" onClick={() => {}} />
-            </div> 
-      </div>
+      <GoogleLogin onSuccess={(credentialResponse) => {
+        console.log(credentialResponse)
+        navigate("/orders");
+      }}
+      onError={() => console.log("Login Error")}></GoogleLogin>
     </>
   )
 }
