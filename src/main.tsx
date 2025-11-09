b3c1c33b3849a6712d1c2c266003cf7dae5392bb
malcolmtroy <malcolmtroy01@tamu.edu>
2025-11-09 15:21:13 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const CLIENT_ID = "428974685395-umtqfn2n5k29vrut98j6ujqumrrpp6gr.apps.googleusercontent.com"
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <GoogleOAuthProvider clientId="428974685395-umtqfn2n5k29vrut98j6ujqumrrpp6gr.apps.googleusercontent.com">
     <App />
     </GoogleOAuthProvider>
   </StrictMode>,
