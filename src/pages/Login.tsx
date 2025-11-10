8b121a41d4407698ccd2a2b9b218a24dd47eb2fa
malcolmtroy <malcolmtroy01@tamu.edu>
2025-11-10 10:03:20 -0600

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -21,7 +21,7 @@ export default function Login() {
             <div className="backgroundBox">
                 <div>
                     <img src="/sharetealogo.png" className="logo" alt="ShareTea logo" />
-                    <GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginError}/>
+                    <div className = "googleButton"> <GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginError}/> </div>
                 </div>
             </div>
       </div>
