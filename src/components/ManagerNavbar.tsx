be4b9f2e0aafefe281e078b5d2393fc5b1ee6530
malcolmtroy <malcolmtroy01@tamu.edu>
2025-12-02 16:44:14 -0600

    Update ManagerNavbar.tsx

--- src/components/ManagerNavbar.tsx
+++ src/components/ManagerNavbar.tsx
@@ -1,4 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export default function ManagerNavbar() {
+  
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+
+    localStorage.clear();
+    sessionStorage.clear();
+
+    console.log("User logged out.");
+
+    navigate("/");
+  };
+  
   return (
     <nav>
       <div className="leftSideNav">
@@ -27,7 +42,7 @@ export default function ManagerNavbar() {
         </div>
 
         <div className="navItem">
-          <button className="logout">Logout</button>
+          <button className="logout" onClick={handleLogout}>Logout</button>
         </div>
       </div>
     </nav>
