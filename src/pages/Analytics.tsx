5031c5639d9700756537e2465d7e0d3012f67e66
malcolmtroy <malcolmtroy01@tamu.edu>
2025-12-04 17:54:36 -0600

    Update Analytics.tsx

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -17,19 +17,6 @@ export default function Analytics() {
   const headerRef = useRef<HTMLElement>(null);
   const [headerH, setHeaderH] = useState(64);
   const [selectedDate, setSelectedDate] = useState("");
-  const [barData, setBarData] = useState({
-    labels: ["9am", "10am"] as string[],
-    datasets: [
-      {
-        label: "Sales ($)",
-        data: [10,20] as number[],
-        backgroundColor: "rgba(197, 48, 48, 0.6)",
-        borderColor: "rgba(197, 48, 48, 1)",
-        borderWidth: 1,
-        borderRadius: 6,
-      },
-    ],
-  });
 
   useLayoutEffect(() => {
     const el = headerRef.current;
@@ -51,6 +38,20 @@ export default function Analytics() {
       document.body.style.overflowY = prev;
     };
   }, []);
+
+  const barData: any = {
+    labels: ["9am", "10am"],
+    datasets: [
+      {
+        label: "Sales ($)",
+        data: [10,20],
+        backgroundColor: "rgba(197, 48, 48, 0.6)",
+        borderColor: "rgba(197, 48, 48, 1)",
+        borderWidth: 1,
+        borderRadius: 6,
+      },
+    ],
+  };
 
   const barOptions: any = {
     responsive: true,
